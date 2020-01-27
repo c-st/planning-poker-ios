2a4ab869c80fa7625d31a10c5d8090ab3b97e833
Edward Byne <edward.byne@codecentric.de>
2020-01-27 17:09:11 +0100

    Add unimplemented pie chart view

---
+++ PlanningPoker/PlanningPoker/View/PieChart/PieChartView.swift
@@ -0,0 +1,21 @@
+//
+//  PieChartView.swift
+//  PlanningPoker
+//
+//  Created by Edward Byne on 27/01/2020.
+//  Copyright © 2020 Christian Stangier. All rights reserved.
+//
+
+import SwiftUI
+
+struct PieChartView: View {
+    var body: some View {
+        Circle()
+    }
+}
+
+struct PieChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        PieChartView()
+    }
+}
