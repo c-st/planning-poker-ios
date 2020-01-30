20d4ee59b9d2632a797d802a0c3420d9e46e5ca4
Edward Byne <edward.byne@codecentric.de>
2020-01-30 16:03:15 +0100

    Refactor segment to separate view

---
+++ PlanningPoker/PlanningPoker/View/Room/Ended/PieChartSegmentView.swift
@@ -0,0 +1,87 @@
+//
+//  PieChartSegmentView.swift
+//  PlanningPoker
+//
+//  Created by Edward Byne on 30/01/2020.
+//  Copyright © 2020 Christian Stangier. All rights reserved.
+//
+
+import SwiftUI
+
+struct PieSegment: Shape {
+    let startAngle: Double
+    let endAngle: Double
+    var radius: Double
+
+    var animatableData: Double {
+        get { return radius }
+        set { radius = newValue }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.addArc(
+                center: CGPoint(x: 100, y: 100),
+                radius: CGFloat(self.radius),
+                startAngle: .degrees(startAngle),
+                endAngle: .degrees(endAngle),
+                clockwise: false
+            )
+            path.addLine(to: CGPoint(x: 100, y: 100))
+            path.closeSubpath()
+        }
+    }
+}
+
+struct SegmentData: Identifiable {
+    let id: UUID = UUID()
+    let startAngle: Double
+    let endAngle: Double
+    let estimators: Int
+    let estimate: String
+
+    var medianRadians: Double {
+        let medianAngle = Angle(degrees: (startAngle + endAngle) / 2)
+        return medianAngle.radians
+    }
+
+    var labelXPosition: CGFloat {
+        return 100 + (70 * CGFloat(cos(medianRadians)))
+    }
+
+    var labelYPosition: CGFloat {
+        return 100 + (70 * CGFloat(sin(medianRadians)))
+    }
+}
+
+struct PieChartSegmentView: View {
+    let data: SegmentData
+    let isSelected: Bool
+    let color: Color
+
+    var body: some View {
+        ZStack {
+            PieSegment(
+                startAngle: data.startAngle,
+                endAngle: data.endAngle,
+                radius: isSelected ? 120.0 : 100.0
+            )
+            .fill(color)
+            .animation(.easeInOut(duration: 0.5))
+
+            if (data.endAngle - data.startAngle) >= 50 {
+                PieChartSegmentLabelView(segmentData: data)
+            }
+        }
+    }
+}
+
+struct PieChartSegmentView_Previews: PreviewProvider {
+    static var previews: some View {
+        PieChartSegmentView(
+            data: SegmentData(startAngle: 0, endAngle: 50, estimators: 3, estimate: "3"),
+            isSelected: false,
+            color: Color.red
+        )
+    }
+}
--- PlanningPoker/PlanningPoker/View/Room/Ended/PieChartView.swift
+++ PlanningPoker/PlanningPoker/View/Room/Ended/PieChartView.swift
@@ -8,54 +8,6 @@
 
 import SwiftUI
 
-struct PieSegment: Shape {
-    let startAngle: Double
-    let endAngle: Double
-    let isSelected: Bool
-    
-    var radius: Double
-    
-    var animatableData: Double {
-        get { return radius }
-        set { radius = newValue }
-    }
-
-    func path(in rect: CGRect) -> Path {
-        Path { path in
-            path.addArc(
-                center: CGPoint(x: 100, y: 100),
-                radius: CGFloat(self.radius),
-                startAngle: .degrees(startAngle),
-                endAngle: .degrees(endAngle),
-                clockwise: false
-            )
-            path.addLine(to: CGPoint(x: 100, y: 100))
-            path.closeSubpath()
-        }
-    }
-}
-
-struct SegmentData: Identifiable {
-    let id: UUID = UUID()
-    let startAngle: Double
-    let endAngle: Double
-    let estimators: Int
-    let estimate: String
-
-    var medianRadians: Double {
-        let medianAngle = Angle(degrees: (startAngle + endAngle) / 2)
-        return medianAngle.radians
-    }
-
-    var labelXPosition: CGFloat {
-        return 100 + (70 * CGFloat(cos(medianRadians)))
-    }
-
-    var labelYPosition: CGFloat {
-        return 100 + (70 * CGFloat(sin(medianRadians)))
-    }
-}
-
 struct PieChartView: View {
     let colors: [Color] = [
         Color.green,
@@ -69,25 +21,21 @@ struct PieChartView: View {
     ]
 
     var segmentData: [SegmentData]
+
+    var segmentIndices: Range<Int> {
+        return 0..<segmentData.count
+    }
+    
     @State var selectedSegmentIndex: Int?
 
     var body: some View {
         ZStack {
-            ForEach(0..<segmentData.count, id: \.self) { index in
-                ZStack {
-                    PieSegment(
-                        startAngle: self.segmentData[index].startAngle,
-                        endAngle: self.segmentData[index].endAngle,
-                        isSelected: index == self.selectedSegmentIndex,
-                        radius: index == self.selectedSegmentIndex ? 120.0 : 100
-                    )
-                    .fill(self.colors[index % self.colors.count])
-                        .animation(.easeInOut(duration: 0.5))
-
-                    if (self.segmentData[index].endAngle - self.segmentData[index].startAngle) >= 50 {
-                        PieChartSegmentLabelView(segmentData: self.segmentData[index])
-                    }
-                }
+            ForEach(segmentIndices, id: \.self) { index in
+                PieChartSegmentView(
+                    data: self.segmentData[index],
+                    isSelected: index == self.selectedSegmentIndex,
+                    color: self.colors[index % self.colors.count]
+                )
                 .gesture(
                     TapGesture().onEnded { _ in
                         self.selectedSegmentIndex = self.selectedSegmentIndex == index ? nil : index
